f43524e6e14b5866d5efdbbe514bd276f3f089b5
vaibgups <vaibgups@gmail.com>
2019-04-30 23:50:32 +0530

    home branch TestActivity add for testing git merge into office

---
+++ app/src/main/java/com/example/comitchat/activity/TestingActivity.java
@@ -0,0 +1,15 @@
+package com.example.comitchat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.comitchat.R;
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
